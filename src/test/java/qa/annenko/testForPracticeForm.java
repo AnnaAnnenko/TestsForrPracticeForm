8eb7adffe6d1ba0b383b651616114a64ff0de396
AnnaAnnenko <annenko.anya@gmail.com>
2022-09-25 15:33:55 +0300

    Create test for Practice Form

--- src/test/java/qa/annenko/testForPracticeForm.java
+++ src/test/java/qa/annenko/testForPracticeForm.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class testForPracticeForm {
@@ -20,18 +22,28 @@ public class testForPracticeForm {
     @Test
     public void checkFieldsOfForm() {
       open("https://demoqa.com/automation-practice-form");
-      $("[id=firstName]").setValue("Anna");
-      $("[id=lastName]").setValue("Abramova");
-      $("[id=userEmail]").setValue("abramova@test.ru");
-//      $("[#gender-radio-2".value=Female]").click();
-
-//      $("[id=dateOfBirthInput]").click();
-//      $("[.react-datepicker__year-select").click();
-//      $("[.react-datepicker__year-select>[value=1996]").click();
-//      $("[.form-control react-datepicker-ignore-onclickoutside]").setValue("18 Sep 1996");
-//      $("[id=dateOfBirthInput]").setValue("18 Sep 1996");
-      $("[id=subjectsContainer]").setValue("subject");
-//      $("[id=lastName]").setValue("Abramova");
-//      $("[id=lastName]").setValue("Abramova");
+      executeJavaScript("$('footer').remove()");
+      executeJavaScript("$('#fixedban').remove()");
+      $("#firstName").setValue("Anna");
+      $("#lastName").setValue("Abramova");
+      $("#userEmail").setValue("abramova@test.ru");
+      $("#gender-radio-2").parent().click();
+      $("#userNumber").setValue("9222365736");
+      $("#dateOfBirthInput").click();
+      $(".react-datepicker__month-select").selectOption("October");
+      $(".react-datepicker__year-select").selectOption("2009");
+      $(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)").click();
+      $("#subjectsInput").setValue("History").pressEnter();
+      $("[for=hobbies-checkbox-2]").click();
+      $("#uploadPicture").uploadFromClasspath("Img/1.png");
+      $("#currentAddress").setValue("Test Current Address");
+      $("#state").click();
+      $("#stateCity-wrapper").$(byText("NCR")).click();
+      $("#city").click();
+      $("#stateCity-wrapper").$(byText("Delhi")).click();
+      $("#submit").click();
+// Проверяем результат
+      $(".modal-dialog").should(appear);
+      $(".table-responsive").shouldHave(text("Anna Abramova"), text("abramova@test.ru"), text("9222365736"));
     }
 }
